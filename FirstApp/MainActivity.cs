23299f7686a952b908c19a9149642cad17a7d866
gregorkookmaa <gregor.kookmaa@tptlive.ee>
2021-10-16 12:03:28 +0300

    First functionalities - Button click effect added

--- FirstApp/MainActivity.cs
+++ FirstApp/MainActivity.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Runtime;
+using Android.Widget;
 using AndroidX.AppCompat.App;
 
 namespace FirstApp
@@ -8,23 +9,29 @@ namespace FirstApp
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        TextView firstText;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            firstText = FindViewById<TextView>(Resource.Id.textView1);
+            var firstButton = FindViewById < Button>(Resource.Id.button1);
+
+            firstText.Text = " Minu esimene tekst";
+            firstButton.Text = "Vajuta mind";
+            firstButton.Click += FirstButton_Click;
+
+
         }
 
-        protected override void OnStart ()
+        private void FirstButton_Click(object sender, System.EventArgs e)
         {
-            base.OnStart();
+            firstText.Text = "Juhuu vajutasid nuppu";
         }
 
-        protected override void OnPause()
-        {
-            base.OnPause();
-        }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
